c434d0392600415d8bdc33e5ec6e9612297162db
Devinder Khroad <dkhroad@gmail.com>
2013-04-21 11:42:08 -0700

    updates

--- chef/cookbooks/osx_workstation/recipes/default.rb
+++ chef/cookbooks/osx_workstation/recipes/default.rb
@@ -6,3 +6,35 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+directory "#{WS_HOME}/bin" do
+   owner "#{WS_USER}"
+end
+
+dmg_package "Virtualbox"  do
+   source "http://download.virtualbox.org/virtualbox/4.2.12/VirtualBox-4.2.12-84980-OSX.dmg"
+   action :install
+   type   "pkg"
+end
+
+dmg_package "Vagrant" do 
+  source "http://files.vagrantup.com/packages/a7853fe7b7f08dbedbc934eb9230d33be6bf746f/Vagrant-1.2.1.dmg"
+  action :install
+  type "pkg" 
+  only_if { !File.exists?("/Applications/Vagrant") }
+end
+
+dmg_package "Sublime Text" do
+   source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203021.dmg"
+   action :install
+   type "pkg"
+end
+
+link "#{WS_HOME}/bin/subl" do
+   to "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+   owner "#{WS_USER}"
+end
+
+#execute "install ack" do
+#command "brew install ack"
+#end
--- chef/solo.rb
+++ chef/solo.rb
@@ -2,6 +2,7 @@ http_proxy  ENV['http_proxy'] || nil
 https_proxy  ENV['http_proxy'] || nil
 no_proxy  ENV['no_proxy'] || nil
 verbose_logging true
+file_cache_path "#{ENV['HOME']}/.chef/cache"
 
 
 cookbook_path File.dirname(File.expand_path(__FILE__)) + "/cookbooks"
